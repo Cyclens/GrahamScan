ff7b8c6dd3d41525c6e34dece3b1f27a3bf8c98d
Mauro Lacy <mauro@lacy.com.ar>
2016-02-07 00:01:36 -0300

    Points in (x, y) natural order

--- GrahamScan.py
+++ GrahamScan.py
@@ -28,7 +28,6 @@ class GrahamScan():
     def __init__(self, pts):
         """
             Computes the convex hull of the specified array of points.
-            Points are in (y, x) format
         """
         self.hull = stack()
 
@@ -38,7 +37,7 @@ class GrahamScan():
         # preprocess so that points[0] has lowest y-coordinate; break ties by x-coordinate
         # points[0] is an extreme point of the convex hull
         # (alternatively, could do easily in linear time)
-        points.sort()
+        points.sort(key=lambda p: p[::-1])
         
         p = points[0]
         def polar(q):
@@ -46,8 +45,8 @@ class GrahamScan():
                 sorts by polar angle with respect to base point points[0],
                 breaking ties by distance (squared) to points[0]
             """
-            dx = q[1] - p[1]
-            dy = q[0] - p[0]
+            dx = q[0] - p[0]
+            dy = q[1] - p[1]
             theta = atan2(dy, dx)
             r2 = dx*dx + dy*dy
             return (theta, r2)
@@ -58,7 +57,7 @@ class GrahamScan():
         equal = True
         k1 = 1
         for k1 in range(1, N):
-            if not (p[0] == points[k1][0] and p[1] == points[k1][1]):
+            if p[0] != points[k1][0] or p[1] != points[k1][1]:
                 equal = False
                 break
         if equal:
@@ -85,7 +84,7 @@ class GrahamScan():
             Is a->b->c a counterclockwise turn?
             Returns { -1, 0, +1 } if a->b->c is a { clockwise, collinear; counterclockwise } turn.
         """
-        area2 = (b[1]-a[1])*(c[0]-a[0]) - (b[0]-a[0])*(c[1]-a[1])
+        area2 = (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
         if area2 < 0:
             return -1
         elif area2 > 0:
@@ -97,10 +96,7 @@ class GrahamScan():
         """
             Returns the extreme points on the convex hull in counterclockwise order.
         """
-        h = []
-        for p in self.hull:
-            h.append(p[::-1])
-        return h
+        return [p for p in self.hull]
 
     # check that boundary of hull is strictly convex
     def is_convex(self):
@@ -127,7 +123,7 @@ if __name__ == '__main__':
     points = list()
     for i in range(N):
         (x, y) = map(int, stdin.readline().split())
-        points.append((y, x))
+        points.append((x, y))
     graham = GrahamScan(points)
     for p in graham.get_hull():
         print p
