f3e947fddf8ef51d2adc00bfda5da855d353e35c
Mauro Lacy <mauro@lacy.com.ar>
2016-02-07 09:54:58 -0300

    Fix stack reference

--- stack.py
+++ stack.py
@@ -1,1 +1,48 @@
-../stack.py
+#!/usr/bin/env python
+#coding: utf
+class stack():
+    def __init__(self):
+        self.stack = []
+        
+    def __len__(self):
+        return len(self.stack)
+    
+    def __repr__(self):
+        return self.stack.__repr__()
+        
+    def __getitem__(self, i):
+        return self.stack[i] 
+    
+    def __iter__(self):
+        for e in self.stack:
+            yield e
+    
+    def push(self, v):
+        self.stack.append(v)
+
+    def peek(self):
+        return self.stack[-1]
+        
+    def pop(self):
+        return self.stack.pop()
+    
+def testStack(A):
+    st = stack()
+    for a in A:
+        st.push(a)
+    return st
+
+TEST = [
+        [(4, 0), (3, 1), (2, 0), (1, 0), (5, 0)],
+        [(1, 0)],
+        [(1, 1)],
+        [(1, 0), (2, 1)],
+        [(1, 1), (2, 0)],
+        [(2, 1), (3, 1), (5, 0)],
+        [(6, 1), (3, 1), (5, 0)],
+       ]
+
+if __name__ == "__main__":
+    from sys import stderr
+    for T in TEST:
+        print "T:", T, ":", testStack(T)
